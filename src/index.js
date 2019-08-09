ad999567b45ccdaff6573f21e059d74a4c02b11c
king-2000 <zs2000>
2019-08-09 09:52:50 +0800

    使用Tab-bar之前

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {HashRouter,Route,Switch} from 'react-router-dom'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './redux/store'
+import Login from './containers/login/login'
+import Register from './containers/register/register'
+import Main from './containers/main/main'
+import Chat from './containers/chat/chat'
+import List from './containers/list/list'
+import Personal from './containers/personal/personal'
+import NotFound from "./components/not-found/not-found";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render((
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+
+                <Route exact path='/login' component={Login}/>
+                <Route path='/register' component={Register}/>
+                <Route path='/chat' component={Chat}/>
+                <Route path='personal' component={Personal}/>
+                <Route path='/list' component={List}/>
+                <Route    component={Main}/>
+                <Route  component={NotFound}/>
+            </Switch>
+        </HashRouter>
+    </Provider>
+), document.getElementById('root'));
+
